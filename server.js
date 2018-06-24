cbdf5a9f8f40c452caa22bf435c3b95d47cfa015
Doncassino <guillaumeks1@gmail.com>
2018-06-24 23:15:45 +0200

    5

--- server.js
+++ server.js
@@ -12,13 +12,20 @@ function endBlink(){
   LED.unexport();
 }
 
+function blinkLED() { //function to start blinking
+    if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
+      LED.writeSync(1); //set pin state to 1 (turn LED on)
+    } else {
+      LED.writeSync(0); //set pin state to 0 (turn LED off)
+    }
+}
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
 
 app.post('/on', function(req, res){
-    LED.writeSync(1);
+    blinkLED();
     console.log('Allumez');
     res.redirect('/');
 })
